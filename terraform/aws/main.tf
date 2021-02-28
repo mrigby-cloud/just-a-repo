740ff82e72cfde4a74536e009b4fa71a663f1986
Matt Rigby <mrigby83@gmail.com>
2021-02-28 19:18:56 +0000

    adding stock check script to root crontab

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -25,6 +25,14 @@ resource "aws_security_group" "stock-check-sec-group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 0
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
